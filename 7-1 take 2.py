bec8cb7f991b37ab86cbe87f1013e8b220e22010
Mitchell Grice <mitch@gricey.net>
2015-12-21 22:09:58 +1000

    Fixed bad indent on 7

--- 7-1 take 2.py
+++ 7-1 take 2.py
@@ -1,26 +1,26 @@
-    cache = {}
-    func_template = '''
-    def {fname}():
-        global cache
-        if "{fname}" not in cache:
-            cache["{fname}"] = {body}
-        return cache["{fname}"]
-    '''
+cache = {}
+func_template = '''
+def {fname}():
+    global cache
+    if "{fname}" not in cache:
+        cache["{fname}"] = {body}
+    return cache["{fname}"]
+'''
 
-    with open('7.in') as f:
-        for line in f:
-            name = "f_" + line.split('->')[1].strip()
-            body = ' '.join(
-                [
-                    "f_" + wire + "()" if wire not in ['AND', 'OR', 'NOT', 'LSHIFT', 'RSHIFT'] and not wire.isdigit()
-                    else wire
-                    for wire in line.split('->')[0].strip().split(' ')
-                ]
-            ).replace('AND', '&')\
-            .replace('OR', '|')\
-            .replace('NOT', '~')\
-            .replace('LSHIFT', '<<')\
-            .replace('RSHIFT', '>>')
-            exec func_template.format(fname=name, body=body)
+with open('7.in') as f:
+    for line in f:
+        name = "f_" + line.split('->')[1].strip()
+        body = ' '.join(
+            [
+                "f_" + wire + "()" if wire not in ['AND', 'OR', 'NOT', 'LSHIFT', 'RSHIFT'] and not wire.isdigit()
+                else wire
+                for wire in line.split('->')[0].strip().split(' ')
+            ]
+        ).replace('AND', '&')\
+        .replace('OR', '|')\
+        .replace('NOT', '~')\
+        .replace('LSHIFT', '<<')\
+        .replace('RSHIFT', '>>')
+        exec func_template.format(fname=name, body=body)
 
-    print f_a()
+print f_a()
