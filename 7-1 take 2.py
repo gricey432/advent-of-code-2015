dd73d7d7203da181f060971eb5ddee0491773619
Mitchell Grice <mitch@gricey.net>
2015-12-21 20:16:43 +1000

    Fixed day 7 retry

--- 7-1 take 2.py
+++ 7-1 take 2.py
@@ -1,19 +1,26 @@
-cache = {}
-func_template = '''
-def {fname}():
-    global cache
-    if {fname} not in cache:
-        cache["{fname}"] = {body}
-    return cache["{fname}"]
-'''
+    cache = {}
+    func_template = '''
+    def {fname}():
+        global cache
+        if "{fname}" not in cache:
+            cache["{fname}"] = {body}
+        return cache["{fname}"]
+    '''
 
-commands = ['AND', 'OR', 'NOT', 'LSHIFT', 'RSHIFT']
-with open('7.in') as f:
-    for l in f:
-        name = "f_" + l.split('->')[1].strip()
-        body = ' '.join(
-            ["f_" + w + "()" if w not in commands and not w.isdigit() else w for w in l.split('->')[0].strip().split(' ')]
-        ).replace('AND', '&').replace('OR', '|').replace('NOT', '~').replace('LSHIFT', '<<').replace('RSHIFT', '>>')
-        exec func_template.format(fname=name, body=body)
+    with open('7.in') as f:
+        for line in f:
+            name = "f_" + line.split('->')[1].strip()
+            body = ' '.join(
+                [
+                    "f_" + wire + "()" if wire not in ['AND', 'OR', 'NOT', 'LSHIFT', 'RSHIFT'] and not wire.isdigit()
+                    else wire
+                    for wire in line.split('->')[0].strip().split(' ')
+                ]
+            ).replace('AND', '&')\
+            .replace('OR', '|')\
+            .replace('NOT', '~')\
+            .replace('LSHIFT', '<<')\
+            .replace('RSHIFT', '>>')
+            exec func_template.format(fname=name, body=body)
 
-print f_a()
+    print f_a()
